6ffbb8fd23176fb8798cb9c7bc6d61dd8cf84987
Himanshu Karma <himanshukarma377@gmail.com>
2026-05-05 01:32:26 +0530

    Initial commit

---
+++ payment-service/payment-service/src/test/java/com/himanshu/resilience/payment_service/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himanshu.resilience.payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
